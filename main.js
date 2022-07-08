60fa3fab504286e93282731f9c482925a1960f8f
Abhigyan Das <103254159+Abhigyan-Das@users.noreply.github.com>
2022-07-08 15:50:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,60 +7,6 @@ var last_position_of_x, last_position_of_y;
     color = "black";
     width_of_line = 1;
 
-    canvas.addEventListener("mousedown", my_mousedown);
-    
-    function my_mousedown(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        //Addictonal Activity ends
-
-        mouseEvent = "mouseDown";
-    }
-
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
-        if (mouseEvent == "mouseDown") {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-        
-        /*
-        Add the function moveTo() with coordinates
-        last_position_of_x, last_position_of_y
-        to start creation of line
-    
-        Add the function lineTo() with coordinates
-        current_position_of_mouse_x, current_position_of_mouse_y
-        to end creation of line
-        */
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-        }
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-
-    }
-
-    canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseEvent = "mouseUP";
-    }
-
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
-
 var last_position_of_touch_x, last_position_of_touch_y;
 
 /*
@@ -114,12 +60,16 @@ function my_touchmove(e)
         current_position_of_mouse_x, current_position_of_mouse_y
         to end creation of line    
     */
+   console.log("Last position of X and Y cordinates");
+   console.log("x = "+ last_position_of_x +"Y = "+ last_position_of_y);
         ctx.moveTo(last_position_of_x, last_position_of_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        console.log("current position of X and Y cordinates");
+   console.log("x = "+ current_position_of_touch_x +"Y = "+ current_position_of_touch_y);
+        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
     ctx.stroke();
 
-    last_position_of_touch_x = current_position_of_touch_x; 
-    last_position_of_touch_y = current_position_of_touch_y;
+    last_position_of_x = current_position_of_touch_x; 
+    last_position_of_y = current_position_of_touch_y;
    
 }
 
